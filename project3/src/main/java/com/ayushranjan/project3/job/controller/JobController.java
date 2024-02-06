52ab761256ee2d627e8961b5de6e68b218411f59
ayush ranjan <asayushranjansinha@gmail.com>
2024-02-06 14:29:03 +0530

    CRUD with mvc

---
+++ project3/src/main/java/com/ayushranjan/project3/job/controller/JobController.java
@@ -0,0 +1,54 @@
+package com.ayushranjan.project3.job.controller;
+
+import com.ayushranjan.project3.job.entity.Job;
+import com.ayushranjan.project3.job.service.JobService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+
+@RequestMapping("/jobs")
+public class JobController {
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
+
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
+        jobService.createJob(job);
+        return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        if (job != null) return new ResponseEntity<>(job, HttpStatus.OK);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJobById(@PathVariable Long id) {
+       boolean deleted = jobService.deleteJobById(id);
+       if(deleted) return new ResponseEntity<>("Job deleted successfully",HttpStatus.OK);
+       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job updatedJob){
+        boolean updated = jobService.updateJobById(id, updatedJob);
+        if(updated) return new ResponseEntity<>("Job updated successfully",HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
